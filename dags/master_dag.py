6b735b43da6fc436629f1d7bc04063431d63fc90
Ananta <milanpaudel784@gmail.com>
2026-07-25 15:03:42 +0545

    added aws_dag_run from master dag

--- dags/master_dag.py
+++ dags/master_dag.py
@@ -2,7 +2,12 @@ from airflow.models.dag import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from pendulum import datetime
 
-with DAG("master_workflow", start_date=datetime(2026, 7, 9), schedule="@daily", catchup=False) as dag:
+with DAG(
+    "master_workflow",
+    start_date=datetime(2026, 7, 9),
+    schedule="0 23 * * 1-5",
+    catchup=False,
+) as dag:
     
     trigger_ingestion = TriggerDagRunOperator(
         task_id="trigger_api_ingestion",
@@ -16,7 +21,15 @@ with DAG("master_workflow", start_date=datetime(2026, 7, 9), schedule="@daily", 
         task_id="trigger_dbt_transformations",
         trigger_dag_id="dbt_stock_transformation_cosmos",
         wait_for_completion=True,
-        deferrable=True  # <-- ALSO GOOD TO HAVE HERE
+        deferrable=True  # <-- ALSO GOOD TO HAVE HERE, 
+        #free your worker while waiting for dbt to finish
     )
 
-    trigger_ingestion >> trigger_warehouse_build
+    trigger_aws_load = TriggerDagRunOperator(
+        task_id="trigger_aws_ecosystem_load",
+        trigger_dag_id="aws_ecosystem_load",
+        wait_for_completion=True,
+        deferrable=True
+    )
+
+    trigger_ingestion >> trigger_warehouse_build >> trigger_aws_load
